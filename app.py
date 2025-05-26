89f467e6f1e6f58efc74ff74fd9414a8a6264a1d
cheong288 <cheonghw@outlook.com>
2025-05-26 16:50:02 +0000

    4

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import os
 import sqlite3
 import datetime
 
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    raise EnvironmentError("GEMINI_API_KEY not found in .env file.")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,6 +28,27 @@ def index():
 @app.route("/paynow", methods=["GET", "POST"])
 def paynow():
     return (render_template("paynow.html"))
+
+@app.route("/prediction", methods=["GET", "POST"])
+def prediction():
+    return (render_template("prediction.html"))
+
+@app.route("/prediction_reply", methods=["GET", "POST"])
+def prediction_reply():
+    q = float(request.form.get("q"))
+    print(q)
+    return (render_template("prediction_reply.html",r=90.2 + (-50.6*q)))
+
+@app.route("/gemini", methods=["GET", "POST"])
+def gemini():
+    return (render_template("gemini.html"))
+
+@app.route("/gemini_reply", methods=["GET", "POST"])
+def gemini_reply():
+    q = request.form.get("q")
+    print(q)
+    r = model.generate_content(q)
+    return (render_template("gemini_reply.html",r=r.text))
 
 @app.route("/add_user", methods=["GET", "POST"])
 def add_user():
